5a02fcdf5be84d3143bb021a69e0430b6daa6564
Chris Schuhmacher <chris@fandom.com>
2020-12-07 08:07:02 -0800

    day 4 part 1

--- day_4/solutions.js
+++ day_4/solutions.js
@@ -1,18 +1,37 @@
 const requireText = require('require-text');
 
-const inputArray = requireText('./input.txt', require).split('\n');
+const input = requireText('./input.txt', require);
 
 /**
- * counts trees at given coordinates in matrix
+ * returns number of valid passports in given array
  *
- * @param Array passports array of passports to check
+ * @param Array data array of passports to check
  * @return {number} Number of valid passports
  */
-function validatePassport(passports) {
+function validatePassport(data) {
 	let validPassports = 0;
+	const passports = data.split('\n\n');
+	const required = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
+
+	for (let i = 0; i < passports.length; i++) {
+		const passport = passports[i];
+		let validPassport = true;
+		
+		for (let j = 0; j < required.length; j++) {
+			const field = required[j];
+
+			if (passport.indexOf(field) < 0) {
+				validPassport = false;
+			}
+		}
+
+		validPassports += validPassport ? 1 : 0;
+	}
 
 	return validPassports;
 }
+
+console.log(validatePassport(input));
 
 module.exports = {
 	validatePassport,
--- test/day_4.spec.js
+++ test/day_4.spec.js
@@ -14,7 +14,7 @@ ecl:brn pid:760753108 byr:1931
 hgt:179cm
 
 hcl:#cfa07d eyr:2025 pid:166559648
-iyr:2011 ecl:brn hgt:59in`.split(' ');
+iyr:2011 ecl:brn hgt:59in`;
 
 describe('Day Four', function() {
   describe('validatePassport', function() {
