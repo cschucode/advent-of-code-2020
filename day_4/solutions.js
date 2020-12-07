b462dca01245c8b2c9735e22939ff7e7446c3fa3
Chris Schuhmacher <chris@fandom.com>
2020-12-07 07:25:52 -0800

    Add day 4

---
+++ day_4/solutions.js
@@ -0,0 +1,39 @@
+const requireText = require('require-text');
+
+const inputArray = requireText('./input.txt', require).split('\n');
+
+/**
+ * counts trees at given coordinates in matrix
+ *
+ * @param Array map matrix array of map coordinates
+ * @param {number} h number of spaces to move right
+ * @param {number} v number of spaces to move down
+ * @return {number} Number of valid passwords
+ */
+function treeCounter(map, h, v) {
+	let treeCount = 0;
+	const xLimit = map[0].length;
+	const yLimit = map.length;
+	let x = h;
+	let y = v;
+
+	while (y < yLimit) {
+		if (map[y][x] === '#') {
+			treeCount++;
+		}
+
+		if (x + h >= xLimit) {
+			x = (x + h) - xLimit;
+		} else {
+			x += h;
+		}
+		y += v;
+	}
+
+	return treeCount;
+}
+
+module.exports = {
+	treeCounter,
+	multipleSlopeTreeCounter,
+}
